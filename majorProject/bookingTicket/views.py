88b40e5c95a20c684a81ce536c4578d1beecebf5
Dipesh Gautam <be2019se674@gces.edu.np>
2024-05-06 16:44:32 +0545

    for loop and if statement

--- majorProject/bookingTicket/views.py
+++ majorProject/bookingTicket/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
+
 
 # Create your views here.
 def index (request):
-    return render(request, 'index.html')
+    book1 = Book()
+    book1.price = 39
+    book1.offer = True
+
+    book2 = Book()
+    book2.price = 55
+    book2.offer = False
+
+    book3 = Book()
+    book3.price = 75
+    book3.offer = True
+
+    books = [book1, book2, book3]
+    return render(request, 'index.html',{'books':books})
